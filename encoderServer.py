e121825ce80616798fe717720032620332711f7b
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2025-03-09 22:46:25 -0400

    Working on getting query embedding through API working.

--- encoderServer.py
+++ encoderServer.py
@@ -8,18 +8,32 @@ import torch
 
 app = FastAPI()
 bi_encoder = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
-# cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2', device='cuda' if torch.cuda.is_available() else 'cpu')
+cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2', device='cuda' if torch.cuda.is_available() else 'cpu')
 
 class InputData(BaseModel):
     sentences: list
 
+class Query(BaseModel):
+    sentences: str
+
 @app.get("/")
 def root():
-   return {"message": "howdy_world"}
+   return {"message": "hi"}
 
-@app.post("/predict")
-def predict(data: InputData):
+@app.post("/biencoder")
+def biencoder(data: InputData):
     corpus_embeddings = bi_encoder.encode(data.sentences, convert_to_tensor=True, show_progress_bar=True).tolist()
     return {"corpus_embeddings" : corpus_embeddings}
+
+@app.post("/crossencoder")
+def crossencoder(data: InputData):
+    cross_encoder_scores = cross_encoder.predict(data).tolist()
+    return {"cross_encoder_scores" : cross_encoder_scores}
+
+@app.post("/q_embed")
+def q_embed(data: Query):
+    question_embedding = bi_encoder.encode(data.query, convert_to_tensor=True).tolist()
+    question_embedding = question_embedding.cuda()
+    return {"question_embedding" : question_embedding}
 
 # Run using: uvicorn filename:app --host 0.0.0.0 --port 8000
--- requesttest.py
+++ requesttest.py
@@ -1,26 +1,28 @@
 from langchain_community.document_loaders.csv_loader import CSVLoader
+from sentence_transformers import util
 import sys
 import csv
-from tqdm.autonotebook import tqdm
-import torch
-import ollama
 import pickle
 from pathlib import Path
 import requests
+import ollama
+
+# maxInt = sys.maxsize
+
+# while True:
+#     # decrease the maxInt value by factor 10 
+#     # as long as the OverflowError occurs.
+#     try:
+#         csv.field_size_limit(maxInt)
+#         break
+#     except OverflowError:
+#         maxInt = int(maxInt/10)
+
+# def encoder_api(url: str, data):
+#     requests.post(url, json=data)
 
 
-maxInt = sys.maxsize
-
-while True:
-    # decrease the maxInt value by factor 10 
-    # as long as the OverflowError occurs.
-    try:
-        csv.field_size_limit(maxInt)
-        break
-    except OverflowError:
-        maxInt = int(maxInt/10)
-
-def encode_csv(path):
+def encode_csv(path: str):
     # Load PDF documents
     loader = CSVLoader(file_path=path,
         csv_args={
@@ -43,41 +45,75 @@ for doc in docs:
 
 root = Path().resolve()
 embedding_cache_path = root / 'embeddings' / 'doc_embedding.pickle'
+embedding_cache_path.parent.mkdir(parents=True, exist_ok=True)
+
 
 if not embedding_cache_path.exists():
     corpus_sentences = ...
     print("Encoding the corpus. This might take a while")
 
     data = {"sentences" : passages}
-    corpus_embeddings = requests.post("http://127.0.0.1:8000/predict", json=data)
+    corpus_embeddings_request = requests.post("http://127.0.0.1:8000/biencoder", json=data)
+    corpus_embeddings = corpus_embeddings_request.json()["corpus_embeddings"]
 
-    # print(corpus_embeddings.json()['detail'])
-    print(corpus_embeddings.json())
+    print("Storing file on disc")
+    with open(embedding_cache_path, "wb") as fOut:
+        pickle.dump({'sentences': corpus_sentences, 'embeddings': corpus_embeddings}, fOut)
 
-    # print("Storing file on disc")
-    # print({'sentences': corpus_sentences, 'embeddings': corpus_embeddings})
-    # with open(embedding_cache_path, "wb") as fOut:
-    #     pickle.dump({'sentences': corpus_sentences, 'embeddings': corpus_embeddings}, fOut)
-
-
-
-
-
-
+else:
+    print("Loading pre-computed embeddings from disc")
+    with open(embedding_cache_path, "rb") as fIn:
+        cache_data = pickle.load(fIn)
+        corpus_sentences = cache_data['sentences']
+        corpus_embeddings = cache_data['embeddings']
 
 
+# This function will search all wikipedia articles for passages that
+# answer the query
+def search(query):
+    print("Input question:", query)
 
+    ##### Semantic Search #####
+    # Encode the query using the bi-encoder and find potentially relevant passages
+    data = {"query" : query}
+    question_embedding = requests.post("http://127.0.0.1:8000/q_embed", json=data)
+    hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=5)
+    hits = hits[0]  # Get the hits for the first query
 
+    ##### Re-Ranking #####
+    # Now, score all retrieved passages with the cross_encoder
+    cross_inp = [[query, passages[hit['corpus_id']]] for hit in hits]
+    data = {"cross_input" : cross_inp}
+    cross_scores = requests.post("http://127.0.0.1:8000/crossencoder", json=data)
 
+    # Sort results by the cross-encoder scores
+    for idx in range(len(cross_scores)):
+        hits[idx]['cross-score'] = cross_scores[idx]
 
+    # Output of top-5 hits from bi-encoder
+    print("\n-------------------------\n")
+    print("Top-N Bi-Encoder Retrieval hits")
+    hits = sorted(hits, key=lambda x: x['score'], reverse=True)
+    for hit in hits[0:100]:
+        print("\t{:.3f}\t{}".format(hit['score'], passages[hit['corpus_id']].replace("\n", " ")))
 
+    # Output of top-5 hits from re-ranker
+    print("\n-------------------------\n")
+    print("Top-N Cross-Encoder Re-ranker hits")
+    hits = sorted(hits, key=lambda x: x['cross-score'], reverse=True)
+    result = list()
+    for hit in hits[0:5]:
+        print("\t{:.3f}\t{}".format(hit['cross-score'], passages[hit['corpus_id']].replace("\n", " ")))
+        result.append(passages[hit['corpus_id']].replace("\n", " "))
+    return(result)
 
+# generate a response combining the prompt and data we retrieved in step 2
+query = "I inserted a malfunction and the overcurrent trip did not occur when it was supposed to?"
+data = search(query=query)
+client = ollama.Client(host='http://localhost:11434')
 
-
-# data = {"sentences" : passages}
-# response = requests.post("http://127.0.0.1:8000/predict", json=data)
-
-
-
-
-# print(response.json())
+model = "llama3.2:latest"
+print(f"\n------------------------ Start {model} Response ------------------------ \n")
+output = client.chat(model=model, messages=[{'role': 'user', 'content': f"Using this data: {data}. Respond to this prompt: {query}"}])
+print(output.message.content)
+print(f"\n------------------------- End {model} Response ------------------------- \n")
