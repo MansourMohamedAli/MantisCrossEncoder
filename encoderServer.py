eb7e60c7b8b61005dd1ac03a9c1e6154b74ff9af
Mohamed Mansour <MansourMohamedAli1996@gmail.com>
2025-03-16 22:23:17 -0400

    Continue work on encoder API

--- encoderServer.py
+++ encoderServer.py
@@ -1,7 +1,7 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-from sentence_transformers import SentenceTransformer, CrossEncoder
+from sentence_transformers import SentenceTransformer, CrossEncoder, util
 from fastapi import FastAPI
 from pydantic import BaseModel
 import torch
@@ -13,8 +13,10 @@ cross_encoder = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2', device='cud
 class InputData(BaseModel):
     sentences: list
 
-class Query(BaseModel):
-    sentences: str
+class Search(BaseModel):
+    query: str
+    embeddings : list
+    passages : list
 
 @app.get("/")
 def root():
@@ -25,15 +27,39 @@ def biencoder(data: InputData):
     corpus_embeddings = bi_encoder.encode(data.sentences, convert_to_tensor=True, show_progress_bar=True).tolist()
     return {"corpus_embeddings" : corpus_embeddings}
 
-@app.post("/crossencoder")
-def crossencoder(data: InputData):
-    cross_encoder_scores = cross_encoder.predict(data).tolist()
-    return {"cross_encoder_scores" : cross_encoder_scores}
-
-@app.post("/q_embed")
-def q_embed(data: Query):
-    question_embedding = bi_encoder.encode(data.query, convert_to_tensor=True).tolist()
+@app.post("/invoke_llm")
+def invoke_llm(input: Search):
+    question_embedding = bi_encoder.encode(input.query, convert_to_tensor=True)
     question_embedding = question_embedding.cuda()
-    return {"question_embedding" : question_embedding}
+
+    hits = util.semantic_search(question_embedding, input.embeddings, top_k=5)
+    hits = hits[0]  # Get the hits for the first query
+
+    ##### Re-Ranking #####
+    # Now, score all retrieved passages with the cross_encoder
+    cross_inp = [[input.query, input.passages[hit['corpus_id']]] for hit in hits]
+    cross_scores = cross_encoder.predict(cross_inp)
+
+    # Sort results by the cross-encoder scores
+    for idx in range(len(cross_scores)):
+        hits[idx]['cross-score'] = cross_scores[idx]
+
+    # Output of top-5 hits from bi-encoder
+    print("\n-------------------------\n")
+    print("Top-N Bi-Encoder Retrieval hits")
+    hits = sorted(hits, key=lambda x: x['score'], reverse=True)
+    for hit in hits[0:100]:
+        print("\t{:.3f}\t{}".format(hit['score'], input.passages[hit['corpus_id']].replace("\n", " ")))
+
+    # Output of top-5 hits from re-ranker
+    print("\n-------------------------\n")
+    print("Top-N Cross-Encoder Re-ranker hits")
+    hits = sorted(hits, key=lambda x: x['cross-score'], reverse=True)
+    result = list()
+    for hit in hits[0:5]:
+        print("\t{:.3f}\t{}".format(hit['cross-score'], input.passages[hit['corpus_id']].replace("\n", " ")))
+        result.append(input.passages[hit['corpus_id']].replace("\n", " "))
+    return({"rerank_results" : result})
+
 
 # Run using: uvicorn filename:app --host 0.0.0.0 --port 8000
--- requesttest.py
+++ requesttest.py
@@ -1,26 +1,10 @@
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from sentence_transformers import util
 import sys
 import csv
 import pickle
 from pathlib import Path
 import requests
 import ollama
-
-# maxInt = sys.maxsize
-
-# while True:
-#     # decrease the maxInt value by factor 10 
-#     # as long as the OverflowError occurs.
-#     try:
-#         csv.field_size_limit(maxInt)
-#         break
-#     except OverflowError:
-#         maxInt = int(maxInt/10)
-
-# def encoder_api(url: str, data):
-#     requests.post(url, json=data)
-
 
 def encode_csv(path: str):
     # Load PDF documents
@@ -35,85 +19,59 @@ def encode_csv(path: str):
     documents = loader.load()
     return documents
 
-docs = encode_csv("data/mantis.csv")
+def generate_passages(data_path):
+    docs = encode_csv(data_path)
+    passages = []
+    for doc in docs:
+        passages.append(str([doc.metadata['Problem Summary'], doc.metadata['Problem Description'], doc.metadata['Notes & Resolution']]))
+    return passages
 
-passages = []
-for doc in docs:
-    # print(doc.metadata['Problem Summary'], doc.metadata['Problem Description'], doc.metadata['Notes & Resolution'])
-    passages.append(str([doc.metadata['Problem Summary'], doc.metadata['Problem Description'], doc.metadata['Notes & Resolution']]))
+def generate_embeddings(passages, embedding_name: str):
+    root = Path().resolve()
+    embedding_cache_path = root / 'embeddings' / embedding_name
+    embedding_cache_path.parent.mkdir(parents=True, exist_ok=True)
 
+    if not embedding_cache_path.exists():
+        corpus_sentences = ...
+        print("Encoding the corpus. This might take a while")
 
-root = Path().resolve()
-embedding_cache_path = root / 'embeddings' / 'doc_embedding.pickle'
-embedding_cache_path.parent.mkdir(parents=True, exist_ok=True)
+        data = {"sentences" : passages}
+        corpus_embeddings_request = requests.post("http://127.0.0.1:8000/biencoder", json=data)
+        corpus_embeddings = corpus_embeddings_request.json()["corpus_embeddings"]
 
-
-if not embedding_cache_path.exists():
-    corpus_sentences = ...
-    print("Encoding the corpus. This might take a while")
-
-    data = {"sentences" : passages}
-    corpus_embeddings_request = requests.post("http://127.0.0.1:8000/biencoder", json=data)
-    corpus_embeddings = corpus_embeddings_request.json()["corpus_embeddings"]
-
-    print("Storing file on disc")
-    with open(embedding_cache_path, "wb") as fOut:
-        pickle.dump({'sentences': corpus_sentences, 'embeddings': corpus_embeddings}, fOut)
-
-else:
-    print("Loading pre-computed embeddings from disc")
-    with open(embedding_cache_path, "rb") as fIn:
-        cache_data = pickle.load(fIn)
-        corpus_sentences = cache_data['sentences']
-        corpus_embeddings = cache_data['embeddings']
-
-
-# This function will search all wikipedia articles for passages that
-# answer the query
-def search(query):
-    print("Input question:", query)
-
-    ##### Semantic Search #####
-    # Encode the query using the bi-encoder and find potentially relevant passages
-    data = {"query" : query}
-    question_embedding = requests.post("http://127.0.0.1:8000/q_embed", json=data)
-    hits = util.semantic_search(question_embedding, corpus_embeddings, top_k=5)
-    hits = hits[0]  # Get the hits for the first query
-
-    ##### Re-Ranking #####
-    # Now, score all retrieved passages with the cross_encoder
-    cross_inp = [[query, passages[hit['corpus_id']]] for hit in hits]
-    data = {"cross_input" : cross_inp}
-    cross_scores = requests.post("http://127.0.0.1:8000/crossencoder", json=data)
-
-    # Sort results by the cross-encoder scores
-    for idx in range(len(cross_scores)):
-        hits[idx]['cross-score'] = cross_scores[idx]
-
-    # Output of top-5 hits from bi-encoder
-    print("\n-------------------------\n")
-    print("Top-N Bi-Encoder Retrieval hits")
-    hits = sorted(hits, key=lambda x: x['score'], reverse=True)
-    for hit in hits[0:100]:
-        print("\t{:.3f}\t{}".format(hit['score'], passages[hit['corpus_id']].replace("\n", " ")))
-
-    # Output of top-5 hits from re-ranker
-    print("\n-------------------------\n")
-    print("Top-N Cross-Encoder Re-ranker hits")
-    hits = sorted(hits, key=lambda x: x['cross-score'], reverse=True)
-    result = list()
-    for hit in hits[0:5]:
-        print("\t{:.3f}\t{}".format(hit['cross-score'], passages[hit['corpus_id']].replace("\n", " ")))
-        result.append(passages[hit['corpus_id']].replace("\n", " "))
-    return(result)
+        print("Storing file on disc")
+        with open(embedding_cache_path, "wb") as fOut:
+            pickle.dump({'sentences': corpus_sentences, 'embeddings': corpus_embeddings}, fOut)
+    else:
+        print("Loading pre-computed embeddings from disc")
+        with open(embedding_cache_path, "rb") as fIn:
+            cache_data = pickle.load(fIn)
+            corpus_sentences = cache_data['sentences']
+            corpus_embeddings = cache_data['embeddings']
+    return corpus_embeddings
 
 # generate a response combining the prompt and data we retrieved in step 2
 query = "I inserted a malfunction and the overcurrent trip did not occur when it was supposed to?"
-data = search(query=query)
-client = ollama.Client(host='http://localhost:11434')
+passages = generate_passages(data_path="data/mantis.csv")
+embeddings = generate_embeddings(passages, embedding_name='doc_embedding.pickle')
 
-model = "llama3.2:latest"
-print(f"\n------------------------ Start {model} Response ------------------------ \n")
-output = client.chat(model=model, messages=[{'role': 'user', 'content': f"Using this data: {data}. Respond to this prompt: {query}"}])
-print(output.message.content)
-print(f"\n------------------------- End {model} Response ------------------------- \n")
+llm_payload = {"query" : query, "passages" : passages, "embeddings" : embeddings }
+
+# data = search(query, embeddings, passages)
+
+rerank_results_json = requests.post("http://127.0.0.1:8000/invoke_llm", json=llm_payload)
+rerank_results = rerank_results_json.json()["rerank_results"]
+print(rerank_results)
+
+
+
+
+# client = ollama.Client(host='http://localhost:11434')
+
+
+
+# model = "llama3.2:latest"
+# print(f"\n------------------------ Start {model} Response ------------------------ \n")
+# output = client.chat(model=model, messages=[{'role': 'user', 'content': f"Using this data: {data}. Respond to this prompt: {query}"}])
+# print(output.message.content)
+# print(f"\n------------------------- End {model} Response ------------------------- \n")
